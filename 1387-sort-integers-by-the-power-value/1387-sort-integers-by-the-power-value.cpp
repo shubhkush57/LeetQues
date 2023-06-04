2593723cf21caca72d898258389ebee53ae9a6bf
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-04 12:32:41 +0530

    Time: 173 ms (34.13%), Space: 47.3 MB (23.78%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -2,25 +2,28 @@ class Solution {
 public:
     #define pp pair<int,int>
     const int m = 1e5;
-    int solve(int n){
+    int solve(int n,unordered_map<int,int>&dp){
         if(n == 1){
             return 0;
         }
-        // if(dp[n] != -1)return dp[n];
+        if(dp.count(n)){
+            return dp[n];
+        }
         int ans = 0;
         if(n%2 == 0){
-            ans = 1+solve(n/2);
+            ans = 1+solve(n/2,dp);
         }
         else {
-            ans = 1+solve(3*n+1);
+            ans = 1+solve(3*n+1,dp);
         }
-        return  ans;
+        return dp[n] = ans;
     }
     int getKth(int lo, int hi, int k) {
         vector<pp>v;
         // vector<int>dp(m,-1);
+        unordered_map<int,int>dp;
         for(int i = lo;i<=hi;i++){
-            int t = solve(i);
+            int t = solve(i,dp);
             v.push_back({t,i});
         }
         sort(v.begin(),v.end());
