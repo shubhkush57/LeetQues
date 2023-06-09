22c267b191d1f39e1983883cfab8c40168a5ff11
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-10 01:22:32 +0530

    Time: 19 ms (99.82%), Space: 89.2 MB (82.68%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
-    // medain of two sorted arrays...
-    int p1 = 0;
-    int p2 = 0;
-    int getmin(vector<int>&nums1,vector<int>&nums2){
-        int m = nums1.size();
-        int n = nums2.size();
-        if(p1<m && p2<n){
-            return nums1[p1]<nums2[p2]?nums1[p1++]:nums2[p2++];
-        }
-        else if(p1<m){
-            return nums1[p1++];
-        }
-        else if(p2<n){
-            return nums2[p2++];
-        }
-        return -1;
-    }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m= nums1.size();
+        int m = nums1.size();
         int n = nums2.size();
         int sum = m+n;
         if(sum%2 == 0){
-            for(int i = 0;i<sum/2-1;i++){
-                int t = getmin(nums1,nums2);
+            return 1.0*(solve(nums1,nums2,0,m-1,0,n-1,sum/2)+solve(nums1,nums2,0,m-1,0,n-1,sum/2-1))/2;
+        }
+        return solve(nums1,nums2,0,m-1,0,n-1,sum/2);
+    }
+    
+    int solve(vector<int>&nums1,vector<int>&nums2,int n1s,int n1e,int n2s,int n2e,int k){
+        if(n1e<n1s){
+            return nums2[k-n1s];
+        }
+        if(n2e<n2s){
+            return nums1[k-n2s];
+        }
+        // now we will be getting middle indexss.
+        int a = (n1s+n1e)/2;
+        int b = (n2s+n2e)/2;
+        int ele1 = nums1[a];
+        int ele2 = nums2[b];
+        if((a+b)<k){
+            // right..
+            
+            if(ele1>ele2){
+                return solve(nums1,nums2,n1s,n1e,b+1,n2e,k);
             }
-            return (getmin(nums1,nums2)+getmin(nums1,nums2))/2.0;
+            else{
+                return solve(nums1,nums2,a+1,n1e,n2s,n2e,k);
+            }
         }
         else{
-            for(int i = 0;i<sum/2;i++){
-                getmin(nums1,nums2);
+            
+            if(ele1>ele2){
+                return solve(nums1,nums2,n1s,a-1,n2s,n2e,k);
             }
-            return getmin(nums1,nums2);
+            else{
+                return solve(nums1,nums2,n1s,n1e,n2s,b-1,k);
+            }
         }
+        
         return -1;
     }
 };
