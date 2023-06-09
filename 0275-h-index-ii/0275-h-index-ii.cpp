d0b7e300989a974797e58793a7a332e90fd0b19f
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-09 21:28:25 +0530

    Time: 25 ms (31.02%), Space: 18.5 MB (73.88%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -8,14 +8,26 @@ public:
         while(start<=end){
             int h = start+(end-start)/2;
             // this is the ciation.. // if there are h reach papter with atlest h cittios..
-            int i = 0;
-            for(;i<n;i++){
-                if(citations[i]>=h){
-                    break;
+            int i = -1;
+            int s = 0;
+            int e = n-1;
+            while(s<=e){
+                int mid = s+(e-s)/2;
+                if(citations[mid]>=h){
+                    e= mid-1;
+                    i = mid;
+                }
+                else{
+                    s = mid+1;
                 }
             }
-            int total = (n-i);
-            if(total >=h){
+            // for(;i<n;i++){
+            //     if(citations[i]>=h){
+            //         break;
+            //     }
+            // }
+            // int total = (n-i);
+            if(i != -1 && (n-i)>=h){
                 ans = h;
                 start = h+1;
             }
