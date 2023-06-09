d662c9adbe6f9aa52b2f234dcd5a6a94591e67b6
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-09 21:26:03 +0530

    Time: 36 ms (5.04%), Space: 18.8 MB (29.97%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -3,22 +3,24 @@ public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
         int ans = 0;
-        for(int i = 1;i<=n;i++){
-           int s = 0;
-            int e = n-1;
-            int indx = -1;
-            while(s<=e){
-                int mid = s+(e-s)/2;
-                if(citations[mid]>=i){
-                    indx = mid;
-                    e = mid-1;
-                }
-                else{
-                    s = mid+1;
+        int start = 0;
+        int end = INT_MAX;
+        while(start<=end){
+            int h = start+(end-start)/2;
+            // this is the ciation.. // if there are h reach papter with atlest h cittios..
+            int i = 0;
+            for(;i<n;i++){
+                if(citations[i]>=h){
+                    break;
                 }
             }
-            if(indx != -1 && (n-indx)>= i){
-                ans = i;
+            int total = (n-i);
+            if(total >=h){
+                ans = h;
+                start = h+1;
+            }
+            else{
+                end = h-1;
             }
         }
         return ans;
