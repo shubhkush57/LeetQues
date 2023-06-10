4851182f5c82dd15a00a3f897d19c0916e871066
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-10 23:30:32 +0530

    Time: 49 ms (79.29%), Space: 16.3 MB (91.59%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -2,37 +2,18 @@ class Solution {
 public:
     int countCollisions(string directions) {
         int n = directions.size();
-        // monotonic stack...
-        stack<char>stk;
-        int ans =0;
-        for(int i = 0;i<n;i++){
-            char t= directions[i];
-            if(t == 'R'){
-                stk.push('R');
-            }
-            else if(t == 'L'){
-                if(!stk.empty() && stk.top() == 'S'){
-                    ans++;
-                }
-                else if(stk.empty() || stk.top() == 'L'){
-                    stk.push('L');
-                }
-                else{
-                    int count = 0;
-                     while(!stk.empty() && stk.top() == 'R'){
-                        count++;
-                         stk.pop();
-                    }
-                    stk.push('S');
-                    ans += count+1;
-                }
-            }
-            else{
-                while(!stk.empty() && stk.top() == 'R'){
-                    ans++;
-                    stk.pop();
-                }
-                stk.push('S');
+        int ans = 0;
+        int left = 0, right = n-1;
+        while(left<n && directions[left] == 'L'){
+            left++;
+        }
+        while(right>=0 && directions[right] == 'R' ){
+            right--;
+        }
+        // every car inbetween them is going to collinde expec the staying one..
+        for(int i = left;i<=right && i<n;i++){
+            if(directions[i] != 'S'){
+                ans++;
             }
         }
         return ans;
