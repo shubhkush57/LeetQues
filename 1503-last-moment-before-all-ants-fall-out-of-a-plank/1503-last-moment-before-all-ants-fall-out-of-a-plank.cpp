f80b27cb1dd4aeb7b05344165e371a6a49a4a8cc
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-10 22:53:32 +0530

    Time: 32 ms (29.10%), Space: 22.8 MB (29.63%) - LeetHub

--- 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -3,23 +3,26 @@ public:
     #define pp pair<int,char>
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
         // last ant falss out..
-        vector<pp>v;
+        // vector<pp>v;
+        int ans = 0;
         for(int i = 0;i<left.size();i++){
-            v.push_back({left[i],'L'});
+            // v.push_back({left[i],'L'});
+            ans = max(ans,left[i]);
         }
         for(int i = 0;i<right.size();i++){
-            v.push_back({right[i],'R'});
+            // v.push_back({right[i],'R'});
+            ans = max(ans,n-right[i]);
         }
-        sort(v.begin(),v.end());
-        int ans = 0;
-        for(int i = 0;i<v.size();i++){
-            if(v[i].second == 'R'){
-                ans = max(ans,n-v[i].first);
-            }
-            else{
-                ans = max(ans,v[i].first);
-            }
-        }
+        // sort(v.begin(),v.end());
+        // int ans = 0;
+        // for(int i = 0;i<v.size();i++){
+        //     if(v[i].second == 'R'){
+        //         ans = max(ans,n-v[i].first);
+        //     }
+        //     else{
+        //         ans = max(ans,v[i].first);
+        //     }
+        // }
         return ans;
     }
 };
