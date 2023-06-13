819154ae3030092de8dae83dfc80da2bd02056b1
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-13 17:07:16 +0530

    Time: 4 ms (91.87%), Space: 11.1 MB (90.28%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,31 +17,53 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        // time complexity will be on(n)
-        // and space complexity will be o(n)
-        unordered_map<Node*,Node*>mp;
+        // if in the original list i place the new nodes next to the original nodes.
         Node* temp = head;
-        Node* ptr = new Node(0);
-        Node* ans = ptr;
+        
         while(temp != NULL){
             Node* t = new Node(temp->val);
-            mp[temp] = t;
-            ptr->next = t;
-            temp = temp->next;
-            ptr = ptr->next;
+            Node* tnode = temp->next;
+            temp->next = t;
+            t->next = tnode;
+            temp = tnode;
         }
+        
+        // now i have to assing just the random pointers.
         temp = head;
-        ptr = ans->next;
         while(temp != NULL){
-            if(temp->random == NULL){
-                ptr->random = NULL;
+            temp->next->random = (temp->random == NULL)? NULL: temp->random->next;
+            temp = temp->next->next;
+        }
+        // now i have to just make the required list.
+        // temp = head;
+        // while(temp != NULL){
+        //     cout<<temp->val<<' ';temp = temp->next;
+        // }
+        // cout<<endl;
+        Node* ptr = new Node(0);
+        Node* ans = ptr;
+        Node* re = new Node(0);
+        Node* our = re;
+        temp = head;
+        int count = 0;
+        while(temp != NULL){
+            Node* t = temp->next;
+            if(count%2 == 0){
+                // add it to the re;
+                re->next = temp;
+                re = re->next;
+                re->next = NULL;
             }
             else{
-                ptr->random = mp[(temp->random)];
+                // add it to the ptr;
+                ptr->next = temp;
+                ptr = ptr->next;
+                ptr->next = NULL;
             }
-            ptr = ptr->next;
-            temp = temp->next;
+            count++;
+            temp = t;
         }
+        head = our->next;
         return ans->next;
     }
 };
