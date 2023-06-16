ff7f90514cf50c8cf67da000456a5059852476cc
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-16 19:47:57 +0530

    Time: 319 ms (29.17%), Space: 162.1 MB (16.36%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,6 +1,67 @@
+class TrieNode{
+    public:
+  char val;
+    bool isTerminal;
+    TrieNode* children[26];
+    TrieNode(char val){
+        this->val = val;
+        this->isTerminal = false;
+        for(int i = 0;i<26;i++){
+            this->children[i] = NULL;
+        }
+    }
+};
+class Trie{
+    public:
+  TrieNode* root;
+    Trie(){
+        root = new TrieNode('/');
+    }
+    void insertUtil(TrieNode* root,string &s,int i){
+        int n = s.size();
+        if( root == NULL){
+            return;
+        }
+        if(i ==n){
+            root->isTerminal = true;
+            return;
+        }
+        TrieNode* child;
+        char t = s[i];
+        if(root->children[t-'a'] != NULL){
+            child = root->children[t-'a'];
+        }
+        else{
+            child = new TrieNode(t);
+            root->children[t-'a'] = child;
+        }
+        insertUtil(child,s,i+1);
+        return;
+    }
+    void insert(string s){
+        int i = 0;
+        insertUtil(root,s,i);
+        return;
+    }
+    bool searchUtil(TrieNode* root,string &s,int i){
+        int n = s.size();
+        if(root == NULL){
+            return false;
+        }
+        if(i == n){
+            return root->isTerminal;
+        }
+        return searchUtil(root->children[s[i]-'a'],s,i+1);
+    }
+    bool search(string s){
+        int i = 0;
+        return searchUtil(root,s,i);
+    }
+    
+};
 class Solution {
 public:
-    int solve(string &s,unordered_map<string,int>&mp,int i,vector<int>&dp){
+    int solve(string &s,Trie* trieObj,int i,vector<int>&dp){
         int n = s.size();
         if(i == n){
             return 0;
@@ -11,24 +72,24 @@ public:
         int ans = INT_MAX;
         for(int j = i;j<n;j++){
             string re = s.substr(i,j-i+1);
-            if(mp.count(re)){
-                ans = min(ans,solve(s,mp,j+1,dp));
+            if(trieObj->search(re)){
+                ans = min(ans,solve(s,trieObj,j+1,dp));
             }
             else{
-                ans = min(ans,j-i+1+solve(s,mp,j+1,dp));
+                ans = min(ans,j-i+1+solve(s,trieObj,j+1,dp));
             }
         }
         return dp[i] = ans;
     }
     int minExtraChar(string s, vector<string>& dictionary) {
-        unordered_map<string,int>mp;
+        Trie* trieObj = new Trie();
         for(auto it: dictionary){
-            mp[it]++;
+            trieObj->insert(it);
         }
         int n = s.size();
         vector<int>dp(n+1,-1);
         int i = 0;
-        int ans = solve(s,mp,i,dp);
+        int ans = solve(s,trieObj,i,dp);
         return ans;
     }
 };
