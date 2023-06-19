733f7268b37154f574afc5916e96830bf8c9c0be
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-19 19:27:34 +0530

    Time: 19 ms (79.34%), Space: 20.7 MB (23.24%) - LeetHub

---
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* recoverFromPreorder(string traversal) {
+        int i = 0;
+        TreeNode* root;
+        unordered_map<int,TreeNode*>mp;
+        int n = traversal.size();
+        vector<TreeNode*>vis(1000,NULL);
+        bool firsttime = true;
+        while(i<n){
+            int val = 0;
+            int count = 0;
+            while(i<n && traversal[i] == '-'){
+                count++;
+                i++;
+            }
+            while(i<n && traversal[i] != '-'){
+                val = val*10+(traversal[i]-'0');
+                i++;
+            }
+            TreeNode* temp = new TreeNode(val);
+            if(firsttime){
+                firsttime = false;
+                root = temp;
+            }
+            else{
+                // if(vis[count-1] == NULL || vis[count-1]->left != NULL){
+                //     mp[count-1]->left = temp;
+                //     vis[count-1] = mp[count-1];
+                // }
+                // else{
+                //     mp[count-1]->right  =temp;
+                //     vis[count-1] = NULL;
+                // }
+                if(mp[count-1]->left == NULL){
+                    mp[count-1]->left= temp;
+                }
+                else{
+                    mp[count-1]->right = temp;
+                }
+            }
+            mp[count] = temp;
+        }
+        return root;
+    }
+};
