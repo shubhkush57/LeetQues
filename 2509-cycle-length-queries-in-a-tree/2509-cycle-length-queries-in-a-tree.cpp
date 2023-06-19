e4bf860d0b2d41adda16f3cf43c2611e744aab4b
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-19 21:57:47 +0530

    Time: 290 ms (39.72%), Space: 73.8 MB (40.42%) - LeetHub

--- 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.cpp
+++ 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) {
-        // we have to find the 
         int q = queries.size();
-        int ele = pow(2,n)-1;
-        vector<int>ans(q);
-        for(int i = 0;i<q;i++){
-            int ai = queries[i][0];
-            int bi = queries[i][1];
-            // i will first start the ai and 
-             // -1 is it as infiite distance.
-            unordered_map<int,int>dis;
-            dis[ai] = 0;
-            while(ai>0){
-                int temp = dis[ai];
-                ai = ai/2; // parent ke pass ja raha hoon.
-                dis[ai] = 1+temp;
-            }
-            int distance = 0;
-            if(dis.count(bi)){
-                ans[i] = dis[bi]+1;
-            }
-            else{
-                while(bi>0){
-                    if(dis.count(bi)){
-                        ans[i] = dis[bi]+distance+1;
-                        break;
-                    }
-                    bi = bi/2;
-                    distance++;
+        vector<int>ans;
+        for(auto it: queries){
+            int ai = it[0];
+            int bi = it[1];
+            int dis = 1;
+            while(ai != bi){
+                if(ai<bi){
+                    swap(ai,bi);
                 }
+                ai = ai/2;
+                dis++;
             }
-            // dis.clear();
+            ans.push_back(dis);
         }
         return ans;
     }
