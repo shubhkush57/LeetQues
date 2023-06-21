cde49f8aa6abd1391fe1b15f7bf6d342ffc67b3c
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-21 08:47:16 +0530

    Time: 444 ms (36.12%), Space: 153 MB (39.50%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -25,9 +25,31 @@ public:
         // ai is not elmenent of our sequence.
         // ai is our sequence elemnets if is going to be odd then - and if even +
         int n= nums.size();
-        vector<vector<int2>>dp(n+1,vector<int2>(2,-1));
-        int i = 0;
-        int our = 0;
-        return solve(nums,i,our,dp);
+        vector<vector<int2>>dp(n+1,vector<int2>(2,0));
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        for(int i = n-1;i>=0;i--){
+             dp[i][0] = dp[i+1][0];
+            dp[i][1] = dp[i+1][1];
+            
+            for(int j = 0;j<=1;j++){
+                if(j == 0){
+                    dp[i][0] = max(dp[i][0],nums[i]+dp[i+1][1]);
+                }
+                else{
+                    dp[i][1] = max(dp[i][1],-nums[i]+dp[i+1][0]);
+                }
+            }
+        }
+        // for(auto it: dp){
+        //     for(auto it1: it){
+        //         cout<<it1<<' ';
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[0][0];
+        // int i = 0;
+        // int our = 0;
+        // return solve(nums,i,our,dp);
     }
 };
