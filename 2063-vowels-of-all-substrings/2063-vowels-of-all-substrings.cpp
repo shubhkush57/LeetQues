26f72b4f436fcd8bcff82cc389ab72ccb503b559
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-21 19:45:33 +0530

    Time: 63 ms (5.98%), Space: 11.2 MB (20.92%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     #define int2 long long
-    long long countVowels(string word) {
+    long long countVowels(string s) {
         int2 ans = 0;
         int2 sum = 0;
-        int n = word.size();
+        int n = s.size();
         for(int i = n-1;i>=0;i--){
-            if(word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u'){
+            if(string("aeiou").find(s[i]) != string::npos){
                 sum += (n-i);
             }
             ans += sum;
