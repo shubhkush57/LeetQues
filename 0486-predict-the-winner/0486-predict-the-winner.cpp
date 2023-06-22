64a21d423667b909bccdb75426adf2b416d415d6
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-23 02:14:55 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (57.24%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
+    int dp[20][20][2];
     int solve(vector<int>&nums,int start,int end ,int turn){
         int n = nums.size();
         if(start>end){
             return 0;
+        }
+        if(dp[start][end][turn] != -1){
+            return dp[start][end][turn];
         }
         int ans = INT_MIN;
         if(turn == 0){
@@ -14,12 +18,12 @@ public:
             ans = max(ans,nums[start]-solve(nums,start+1,end,0));
             ans = max(ans,nums[end]-solve(nums,start,end-1,0));
         }
-        return ans;
+        return dp[start][end][turn] = ans;
     }
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        int sum = accumulate(nums.begin(),nums.end(),0);
         int start = 0;
+        memset(dp,-1,sizeof(dp));
         int end = n-1;
         int turn = 0;
         int best = solve(nums,start,end,turn);
