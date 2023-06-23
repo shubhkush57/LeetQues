c6242f85df772b4cbafff8513b598138372859aa
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-23 21:56:19 +0530

    Time: 78 ms (59.37%), Space: 29.2 MB (62.52%) - LeetHub

---
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int dp[40000+5][3];
+    int solve(vector<int>&nums,int i,int sum){
+        int n = nums.size();
+        if(i == n){
+            if(sum == 0){
+                return 0;
+            }
+            return INT_MIN;
+        }
+        if(dp[i][sum] !=-1){
+            return dp[i][sum];
+        }
+        int ans = 0;
+        ans = max(ans,solve(nums,i+1,sum));
+        ans = max(ans,nums[i]+ solve(nums,i+1,(sum%3+(nums[i]%3))%3));
+        return dp[i][sum] = ans;
+    }
+    int maxSumDivThree(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        memset(dp,-1,sizeof(dp));
+        sort(nums.begin(),nums.end());
+        
+        int ans = solve(nums,i,0);
+        return ans;
+    }
+};
