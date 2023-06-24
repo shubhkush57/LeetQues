5dba1589ca779d42871af525367f5d1bf63a8268
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-06-24 06:52:32 +0530

    Added solution - LeetHub

---
+++ Insertion Sort - GFG/insertion-sort.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void insert(int arr[], int i)
+    {
+        //code here
+        for(int j = i;j>=1;j--){
+            if(arr[j]<arr[j-1]){
+                // swap(arr[j],arr[j-1]);
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                
+            }
+            else{
+                break;
+            }
+        }
+        
+        return;
+    }
+     public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        for(int i = 1;i<n;i++){
+            // we have to insert this card into it's right position in the sorted part.
+            insert(arr,i);
+        }
+        return;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
